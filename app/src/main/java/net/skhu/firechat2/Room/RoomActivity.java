3a862ef9e14d5205f0e9361c4fab968421e4aa38
ccomapjw <ccomapjw@naver.com>
2020-06-30 21:01:17 +0900

    pjw

--- app/src/main/java/net/skhu/firechat2/Room/RoomActivity.java
+++ app/src/main/java/net/skhu/firechat2/Room/RoomActivity.java
@@ -689,8 +689,9 @@ public class RoomActivity extends AppCompatActivity {
                 public void onClick(DialogInterface dialog, int index) {
                     // 삭제 작업 실행
 
-
                     closedRoom = true;
+
+                    locationUpdateThread.cancel();
 
                     closeRoomThread = new CloseRoomThread(()->closeRoom());
                     Thread t = new Thread(closeRoomThread,"closeRoomThread");
