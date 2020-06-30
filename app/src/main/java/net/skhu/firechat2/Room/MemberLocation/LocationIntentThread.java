a75be16101d6f316bb067af0b47bcef0a6f54c7d
ccomapjw <ccomapjw@naver.com>
2020-06-30 20:05:18 +0900

    pjw

--- app/src/main/java/net/skhu/firechat2/Room/MemberLocation/LocationIntentThread.java
+++ app/src/main/java/net/skhu/firechat2/Room/MemberLocation/LocationIntentThread.java
@@ -1,11 +1,7 @@
 package net.skhu.firechat2.Room.MemberLocation;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
 
-import net.skhu.firechat2.Item.RoomMemberLocationItem;
 import net.skhu.firechat2.ListenerInterface.RoomLocationListener.OnMapIntentPrepareListener;
 
 public class LocationIntentThread implements Runnable {
@@ -34,16 +30,16 @@ public class LocationIntentThread implements Runnable {
 
         onMapIntentPrepareListener.onMapIntentPrepareListener();
 
-        RoomMemberLocationItem roomMemberLocationItem = activity.roomMemberLocationRecyclerViewAdapter.get(selectIndex);//업데이트 받은 것 저장
-
-        Log.v("pjw", "현재위치 \n위도 " + roomMemberLocationItem.getLatitude() + "\n경도 " + roomMemberLocationItem.getLongitude());
-
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setPackage("com.google.android.apps.maps");
-        //String data = "geo:"+roomMemberLocationItem.getLatitude()+", "+roomMemberLocationItem.getLongitude();
-        String data = LocationFunc.locationDataStr(roomMemberLocationItem.getLatitude(), roomMemberLocationItem.getLongitude());
-        intent.setData(Uri.parse(data));
-        activity.startActivity(intent);
+//        RoomMemberLocationItem roomMemberLocationItem = activity.roomMemberLocationRecyclerViewAdapter.get(selectIndex);//업데이트 받은 것 저장
+//
+//        Log.v("pjw", "현재위치 \n위도 " + roomMemberLocationItem.getLatitude() + "\n경도 " + roomMemberLocationItem.getLongitude());
+//
+//        Intent intent = new Intent();
+//        intent.setAction(Intent.ACTION_VIEW);
+//        intent.setPackage("com.google.android.apps.maps");
+//        //String data = "geo:"+roomMemberLocationItem.getLatitude()+", "+roomMemberLocationItem.getLongitude();
+//        String data = LocationFunc.locationDataStr(roomMemberLocationItem.getLatitude(), roomMemberLocationItem.getLongitude());
+//        intent.setData(Uri.parse(data));
+//        activity.startActivity(intent);
     }
 }
